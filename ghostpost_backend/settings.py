3f57f0b118c867b282b7a1077b56005ffe92badd
dbmiddle <dbmiddle39@gmail.com>
2020-06-24 19:33:46 -0400

    added CORS requirements to settings

--- ghostpost_backend/settings.py
+++ ghostpost_backend/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'api'
+    'api',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ORIGIN_WHITELIST = (
+       'http://localhost:3000',
+)
